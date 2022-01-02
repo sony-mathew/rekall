2f1cdf3ae0453db782f77f0f9e982ec262a3eb9c
sony-mathew <ynos1234@gmail.com>
2022-01-02 15:30:22 +0530

    migration for changing score field to float in snapshot table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_01_170843) do
+ActiveRecord::Schema.define(version: 2022_01_02_095332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -150,7 +150,7 @@ ActiveRecord::Schema.define(version: 2021_08_01_170843) do
     t.string "name"
     t.string "notes"
     t.json "data", null: false
-    t.integer "latest_score"
+    t.float "latest_score"
     t.integer "result_id", null: false
     t.integer "query_id", null: false
     t.integer "query_group_id", null: false
