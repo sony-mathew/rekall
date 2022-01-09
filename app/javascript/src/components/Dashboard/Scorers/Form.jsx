d30bc69eadcf1c48ff64e86388db782f39973eb2
sony-mathew <ynos1234@gmail.com>
2022-01-09 13:08:41 +0530

    Added dropdown for selecting predefined scale type

--- app/javascript/src/components/Dashboard/Scorers/Form.jsx
+++ app/javascript/src/components/Dashboard/Scorers/Form.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import * as yup from "yup";
 import { Formik, Form } from "formik";
-import { Input, Textarea } from "neetoui/formik";
+import { Input, Textarea, Select } from "neetoui/formik";
 import { Button } from "neetoui";
 import scorerService from "apis/scorerService";
 
 export default function NewForm({ onClose, refetch, currentResource }) {
   const handleSubmit = async resource => {
+    if(typeof resource.scale_type === 'object' && resource.scale_type !== null) {
+      resource.scale_type = resource.scale_type.value;
+    }
+
     try {
       if(currentResource) {
         await scorerService.update(currentResource.id, resource);
@@ -23,11 +27,13 @@ export default function NewForm({ onClose, refetch, currentResource }) {
   const getInitialValues = () => {
     const resourceObj = currentResource || {
       name: "",
-      scale_type: "",
+      scale_type: "graded",
       code: ""
     };
     return resourceObj;
   };
+
+  const isCommunityScorer = currentResource && currentResource.id && !currentResource.user_id;
 
   return (
     <Formik
@@ -35,15 +41,25 @@ export default function NewForm({ onClose, refetch, currentResource }) {
       onSubmit={handleSubmit}
       validationSchema={yup.object({
         name: yup.string().required("Name is required"),
-        scale_type: yup.string().required("Scale Type is required"),
         code: yup.string().required("Code is required"),
       })}
     >
       {({ isSubmitting }) => (
         <Form>
-          <Input label="Name" name="name" className="mb-6" />
-          <Input label="Scale Type" name="environment" className="mb-6" />
-          <Textarea label="Scorer Algorithm Code" name="code" rows={8} className="mb-24"/>
+          <Input label="Name" name="name" className="mb-6" disabled={isCommunityScorer} />
+          <Select
+            label="Scale"
+            placeholder="Select an Option"
+            name="scale_type"
+            options={[
+              { value: "binary", label: "Binary" },
+              { value: "graded", label: "Graded" },
+              { value: "detailed", label: "Detailed" },
+            ]}
+            disabled={isCommunityScorer}
+            className="mb-6"
+          />
+          <Textarea label="Scorer Algorithm Code" name="code" rows={8} className="mb-24" disabled={isCommunityScorer}/>
           <div className="nui-pane__footer nui-pane__footer--absolute">
             <Button
               onClick={onClose}
@@ -58,7 +74,7 @@ export default function NewForm({ onClose, refetch, currentResource }) {
               size="large"
               style="primary"
               className="ml-2"
-              disabled={isSubmitting}
+              disabled={isCommunityScorer || isSubmitting}
               loading={isSubmitting}
             />
           </div>
